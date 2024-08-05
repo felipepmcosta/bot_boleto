43af29018f9d2f90e1246214e5205cc6914ae4ec
Maycon-MB <mayconbruno.constancio@gmail.com>
2024-08-05 09:52:50 -0300

    Atualizado 05/08/2024

--- db.py
+++ db.py
@@ -1,76 +1,105 @@
 import psycopg2
 import logging
 
-# Configurando o sistema de logging para o módulo db.py
+# Configuração de logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-class Database:
-    def __init__(self):
-        self.conn = psycopg2.connect(
+def atualizar_envio(mat):
+    try:
+        conn = psycopg2.connect(
             dbname="BOLETOS",
             user="postgres",
             password="postgres",
             host="192.168.1.163"
         )
-        self.cursor = self.conn.cursor()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE boletos_geral SET envio = now() WHERE mat = %s", (mat,))
+        conn.commit()
+    except psycopg2.Error as e:
+        logger.error("Erro ao atualizar coluna 'envio' no PostgreSQL: %s", e)
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
 
-    def close(self):
-        self.cursor.close()
-        self.conn.close()
+def extrair_emails(campos_email):
+    emails = []
+    for campo in campos_email:
+        if campo:
+            emails.extend(campo.split(','))
+    return [email.strip() for email in emails if email.strip()]
 
-    def inserirToken(self, mat, token):
-        try:
-            self.cursor.execute("UPDATE boletos_geral SET token = %s WHERE mat = %s AND token IS NULL AND boleto IS NOT NULL", (token, mat))
-        except psycopg2.Error as e:
-            logger.error("Erro ao inserir token no PostgreSQL: %s", e)
+def pega_contatos_db(mat_prefix=None, cot_prefix=None):
+    contatos = []
+    conn = None
+    cursor = None
+    try:
+        conn = psycopg2.connect(
+            dbname="BOLETOS",
+            user="postgres",
+            password="postgres",
+            host="192.168.1.163"
+        )
+        cursor = conn.cursor()
 
-    def commit(self):
-        self.conn.commit()
+        if mat_prefix is not None and cot_prefix is not None:
+            cursor.execute("SELECT * FROM boletos_geral WHERE mat = %s AND LEFT(mat, 2) = %s AND cot = %s AND envio is NULL", (mat_prefix, cot_prefix))
+        else:
+            cursor.execute("SELECT * FROM boletos_geral WHERE mat = %s AND envio is NULL")
 
-    def atualizar_envio(self, mat):
-        try:
-            self.cursor.execute("UPDATE boletos_geral SET envio = now() WHERE mat = %s", (mat,))
-        except psycopg2.Error as e:
-            logger.error("Erro ao atualizar coluna 'envio' no PostgreSQL: %s", e)
+        rows = cursor.fetchall()
 
-    def pega_contatos_db(self, mat_prefix=None, cot_prefix=None):
-        contatos = []
-        try:
-            if mat_prefix is not None and cot_prefix is not None:
-                self.cursor.execute("SELECT id, mat, nome, cot, boleto, digitavel, token, envio, geracao, created_at, updated_at, email, cpfa, cpf, cpf2, pix FROM boletos_geral WHERE LEFT(mat, 2) = %s AND cot = %s AND envio is NULL LIMIT 10", (mat_prefix, cot_prefix))
-            else:
-                self.cursor.execute("SELECT id, mat, nome, cot, boleto, digitavel, token, envio, geracao, created_at, updated_at, email, cpfa, cpf, cpf2, pix FROM boletos_geral WHERE envio is NULL LIMIT 10")
-            rows = self.cursor.fetchall()
-            for row in rows:
+        # Encontrar a data mais recente entre os resultados
+        data_mais_recente = max(row[9] for row in rows) if rows else None
+
+        # Iterar sobre os resultados e filtrar apenas os registros com a data mais recente
+        for row in rows:
+            id = row[0]
+            mat = row[1]
+            nome = row[2]
+            cot = row[3]
+            boleto = row[4]
+            digitavel = row[5]
+            token = row[6]
+            envio = row[7]
+            geracao = row[8]
+            created_at = row[9]
+            updated_at = row[10]
+            email = row[11]
+            cpfa = row[12]
+            cpf = row[13]
+            cpf2 = row[14]
+            pix = row[15]
+
+            # Verificar se a data gerada é a mais recente
+            if created_at == data_mais_recente:
                 contatos.append({
-                    'id': row[0],
-                    'mat': row[1],
-                    'nome': row[2],
-                    'cot': row[3],
-                    'boleto': row[4],
-                    'digitavel': row[5],
-                    'token': row[6],
-                    'envio': row[7],
-                    'geracao': row[8],
-                    'created_at': row[9],
-                    'updated_at': row[10],
-                    'email': row[11],
-                    'cpfa': row[12],
-                    'cpf': row[13],
-                    'cpf2': row[14],
-                    'pix': row[15]
+                    'id': id,
+                    'mat': mat,
+                    'nome': nome,
+                    'cot': cot,
+                    'boleto': boleto,
+                    'digitavel': digitavel,
+                    'token': token,
+                    'envio': envio,
+                    'geracao': geracao,
+                    'created_at': created_at,
+                    'updated_at': updated_at,
+                    'email': email,
+                    'cpfa': cpfa,
+                    'cpf': cpf,
+                    'cpf2': cpf2,
+                    'pix': pix
                 })
-        except psycopg2.Error as e:
-            logger.error("Erro ao conectar ao PostgreSQL: %s", e)
+    
+    except psycopg2.Error as e:
+        logger.error("Erro ao conectar ao PostgreSQL: %s", e)
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
 
-        return contatos
-
-    @staticmethod
-    def extrairEmails(camposEmail):
-        emails = []
-        for campo in camposEmail:
-            if campo:
-                emails.extend(campo.split(','))
-
-        return [email.strip() for email in emails if email.strip()]
+    return contatos
--- enviarBoletos.py
+++ enviarBoletos.py
@@ -1,49 +1,50 @@
-# enviaBoletos.py
-import os, sys
-import smtplib
-import logging
-import datetime
-import json
+import os, smtplib, logging, hashlib, datetime, re, traceback, sys, requests
 from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email_validator import validate_email
-from db import Database
+from email_validator import validate_email, EmailNotValidError
+from db import pega_contatos_db, atualizar_envio
 
-# Configurando o sistema de logging
-logging.basicConfig(filename='email_logs.log', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+# Obtendo os argumentos da linha de comando
+args = sys.argv[1:]  # Ignora o primeiro argumento, que é o nome do script
 
-# Verificando os argumentos mat_prefix e cot_prefix
-if len(sys.argv) < 3:
+# Verificando se foram fornecidos argumentos suficientes
+if len(args) < 2:
     print("Os argumentos 'mat_prefix' e 'cot_prefix' não foram fornecidos. Enviando boletos para todos os contatos disponíveis...")
     mat_prefix = None
     cot_prefix = None
 else:
     # Atribuindo os argumentos a 'mat_prefix' e 'cot_prefix'
-    mat_prefix = sys.argv[1]
-    cot_prefix = sys.argv[2]
+    mat_prefix = args[0]
+    cot_prefix = args[1]
 
-# Constantes para configurações de e-mail
-SMTP_HOST = 'smtp.smce.rio.br'
-SMTP_PORT = 8025
-SMTP_USERNAME = 'boleto@smce.rio.br'
-SMTP_PASSWORD = 'EchH464%'
+# Obtendo os contatos com base nos dois primeiros dígitos de 'mat' e 'cot' da função pegaContatosDB()
+contatos = pega_contatos_db(mat_prefix, cot_prefix)
+
+# Se não houver contatos disponíveis, exiba uma mensagem e saia
+if not contatos:
+    print("Não foram encontrados contatos para enviar boletins. Todos já foram enviados.")
+    sys.exit(1)
 
 # Constante para o remetente padrão
-EMAIL_SENDER = 'boleto@smce.rio.br'
+EMAIL_SENDER = 'boleto@smrede.com.br'
 
 # Variável de controle para pausar o envio de e-mails
-envioPausado = False
+envio_pausado = False
 
-link = "https://boletos.santamonicarede.com.br/"
+# Configuração do sistema de logging
+logging.basicConfig(filename='email_logs.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-def lerTemplate(filename):
+# Link do Boleto
+link = "https://boleto.santamonicarede.com.br/"  
+
+def ler_template(filename):
     # Função para ler o modelo do e-mail a ser enviado
     with open(filename, 'r', encoding='utf-8') as arquivotemplate:
         return Template(arquivotemplate.read())
 
-def pegaUnidade(matricula):
+def pega_unidade(matricula):
     # Função para obter a unidade com base na matrícula
     unidades = {
         "01": "Bento Ribeiro",
@@ -62,176 +63,91 @@ def pegaUnidade(matricula):
         "17": "Recreio dos Bandeirantes"
     }
     numero = matricula[:2]
-    return unidades.get(numero, "Matriz")
+    return unidades.get(numero, "Sem Unidade selecionada")
 
-def emailsPorUnidade():
-    # Dicionário mapeando cada unidade para seus emails correspondentes
-    emailsUnidade = {
-        "Bento Ribeiro": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Madureira": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Santa Cruz": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Cascadura": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Taquara": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Nilópolis": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Seropédica": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Barra da Tijuca": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Campo Grande": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Mangueira": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Maricá": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Ilha do Governador": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Freguesia": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"],
-        "Recreio dos Bandeirantes": ["maycon.csc@smrede.com.br", "boleto@smce.rio.br"]
-    }
-    return emailsUnidade
-
-def enviarEmail(destinatario, assunto, mensagem, emailsUnidade=None):
+def enviarEmail(destinatario, assunto, mensagem):
     destinatario_temporario = "maycon.csc@smrede.com.br"
+    
     try:
-        # Dividindo vários endereços de e-mail e removendo strings vazias
-        destinatarios = [email.strip() for email in destinatario.split(',') if email.strip()]
-        for destinatario in destinatarios:
-            if not validate_email(destinatario):
-                raise ValueError(f"O email '{destinatario}' não está em um formato válido.")
-        s = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
-        s.starttls()
-        s.login(SMTP_USERNAME, SMTP_PASSWORD)
-        msg = MIMEMultipart()
-        msg['From'] = EMAIL_SENDER
-        msg['To'] = destinatario_temporario
-        if emailsUnidade:
-            msg['Cc'] = ", ".join(emailsUnidade)  # Adiciona os emails da unidade como cópia
-        msg['Subject'] = assunto
-        msg.attach(MIMEText(mensagem, 'html'))
-        s.send_message(msg)
-        s.quit()
-        return True
+        envio_destinatarios = [email.strip() for email in destinatario_temporario.split(',') if email.strip()]
 
-    except (smtplib.SMTPAuthenticationError, smtplib.SMTPException, ValueError) as e:
+        for destinatario_individual in envio_destinatarios:
+            try:
+                # Validação de e-mail
+                v = validate_email(destinatario_individual)
+                email_validado = v.email  # Acessando o e-mail validado corretamente
+            except EmailNotValidError as e:
+                raise ValueError(f"O email '{destinatario_individual}' não está em um formato válido: {str(e)}")
+
+            # Obter a unidade com base na matrícula
+            unidade = pega_unidade(mat)
+
+            # Dados do JSON incorporados no código
+            dados_json = {
+                "para": destinatario_individual,  # Usando cada destinatário individualmente
+                "de": EMAIL_SENDER,  # Certifique-se de definir EMAIL_SENDER com seu endereço de e-mail
+                "assunto": assunto,
+                "html": mensagem,  # Conteúdo HTML do e-mail
+                "categorias": ["Boleto", unidade]
+            }
+
+            # URL da API para enviar e-mail
+            api_url = 'https://sendmail.smrede.tec.br/sendEmail'
+
+            # Executar o envio de e-mail usando a API
+            enviado = sendMessage(dados_json, api_url)
+
+            if enviado:
+                logging.info(f'E-mail enviado para {destinatario_individual} usando API.')
+            else:
+                logging.error(f'Falha ao enviar e-mail para {destinatario_individual} usando API.')
+
+        return True  # Indica que todos os e-mails foram enviados com sucesso
+
+    except (requests.exceptions.RequestException, ValueError) as e:
         logging.error(f'Erro ao enviar o e-mail: {str(e)}')
         return False
 
-def relatorio_por_unidade(envios, tipo_relatorio, destinatario_temporario):
+def sendMessage(dados_json, api_url):
     try:
-        envios_por_unidade = {}
-        for envio in envios:
-            unidade = envio.get('unidade', 'Outros')
-            if unidade not in envios_por_unidade:
-                envios_por_unidade[unidade] = {'sucesso': [], 'problema': []}
-            if tipo_relatorio == "Sucesso":
-                envios_por_unidade[unidade]['sucesso'].append(envio)
-            elif tipo_relatorio == "Problema":
-                envios_por_unidade[unidade]['problema'].append(envio)
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(api_url, headers=headers, json=dados_json)
 
-        # Criando relatório por unidade
-        for unidade, envios_unidade in envios_por_unidade.items():
-            data_hora_atual = datetime.datetime.now()
-            total_sucesso = len(envios_unidade['sucesso'])
-            total_problema = len(envios_unidade['problema'])
-            corpo_email = f"""\
-<html>
-    <head></head>
-    <body>
-        <p>Relatório de Envios - Unidade {unidade}</p>
-        <p>Data do Relatório: {data_hora_atual.strftime("%d/%m/%Y")}</p>
-        <p>Hora do Relatório: {data_hora_atual.strftime("%H:%M:%S")}</p>
-        <p>Total de Envios com Sucesso: {total_sucesso}</p>
-        <p>Total de Envios com Problema: {total_problema}</p>
-"""
-            # Adicionar tabela apenas se houver envios com problema
-            if total_problema > 0:
-                corpo_email += """\
-        <p>Aqui estão os detalhes dos envios com problema:</p>
-        <table border='1' cellpadding='5' style='border-collapse: collapse; text-align: center;'>
-            <tr style='background-color: #120a8f; color: white;'>
-                <th>Quantidade</th>
-                <th>Matrícula</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Data de Envio</th>
-                <th>Status</th>
-"""
-                for idx, envio in enumerate(envios_unidade['problema'], start=1):
-                    corpo_email += f"""\
-            <tr style='background-color: #ffeac4;'>
-                <td>{idx}</td>
-                <td>{envio['matricula']}</td>
-                <td>{envio['nome']}</td>
-                <td>{envio['destinatario']}</td>
-                <td>{envio['data_hora']}</td>
-                <td>Problema</td>
-            </tr>
-"""
-                corpo_email += """\
-        </table>
-"""
-            corpo_email += """\
-    </body>
-</html>
-"""
-            assunto = f"Relatório de Envios - Unidade {unidade}"
-            enviado = enviarEmail(destinatario_temporario, assunto, corpo_email)  # Aqui passamos a mensagem como argumento
-            if enviado:
-                logging.info(f'Relatório de envios para a unidade {unidade} enviado por e-mail.')
-            else:
-                logging.error(f'Falha ao enviar o relatório de envios para a unidade {unidade} por e-mail.')
-    except Exception as ex:
-        logging.error(f'Ocorreu um erro ao gerar os relatórios de envio de e-mail por unidade: {ex}')
+        if response.status_code == 200:
+            logging.info(f'E-mail enviado usando API para {dados_json["para"]}')
+            return True
+        else:
+            logging.error(f'Falha ao enviar e-mail usando API para {dados_json["para"]}. Status code: {response.status_code}')
+            return False
 
-def registro_xml(envios, tipo_relatorio, data_hora):
-    try:
-        # Obtendo a data e hora para incluir no título do arquivo
-        data = data_hora.split()[0]  # Obtém apenas a parte da data
-        hora = data_hora.split()[1]  # Obtém apenas a parte da hora
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Erro ao enviar e-mail usando API: {str(e)}')
+        return False
 
-        # Construindo o nome do arquivo com base na data e hora atuais
-        nome_arquivo = f"relatorio_envios_{data}_{hora}.xml"
+try:
+    contatos = pega_contatos_db(mat_prefix, cot_prefix)
+    mensagem_template = ler_template('msg.html')
+    envio_corretos = []
+    envio_incorretos = []
+    for contato in contatos:
+        if envio_pausado:
+            logging.info('Envio de e-mails pausado.')
+            break
+        nome = contato['nome']
+        mat = contato['mat']
+        unidade = pega_unidade(mat)
+        email = contato['email']
+        token = contato['token']
+        linkToken = f"{link}{token}"
+        mensagem = mensagem_template.substitute(PERSON_NAME=nome.title(), LINK=linkToken)
+        assunto = f"Seu BOLETO SMREDE - Unidade {unidade} chegou!!!"
+        enviado = enviarEmail(email, assunto, mensagem)
+        if enviado:
+            envio_corretos.append({'data_hora': datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 'destinatario': email, 'nome': nome, 'matricula': mat, 'unidade': unidade})
+            atualizar_envio(mat)
+        else:
+            if email not in [envio['destinatario'] for envio in envio_incorretos]:
+                envio_incorretos.append({'destinatario': email, 'nome': nome, 'matricula': mat, 'email': email, 'data_hora': datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 'unidade': unidade})
 
-        # Criando o arquivo XML de relatório
-        with open(nome_arquivo, 'w') as arquivo_xml:
-            arquivo_xml.write("<?xml version='1.0' encoding='UTF-8'?>\n")
-            arquivo_xml.write("<relatorio>\n")
-            for envio in envios:
-                arquivo_xml.write("  <envio>\n")
-                arquivo_xml.write(f"    <matricula>{envio['matricula']}</matricula>\n")
-                arquivo_xml.write(f"    <nome>{envio['nome']}</nome>\n")
-                arquivo_xml.write(f"    <destinatario>{envio['destinatario']}</destinatario>\n")
-                arquivo_xml.write(f"    <data_hora>{envio['data_hora']}</data_hora>\n")
-                arquivo_xml.write(f"    <status>{tipo_relatorio}</status>\n")
-                arquivo_xml.write("  </envio>\n")
-            arquivo_xml.write("</relatorio>\n")
-
-        logging.info(f'Arquivo XML de relatório "{nome_arquivo}" criado com sucesso.')
-    except Exception as ex:
-        logging.error(f'Ocorreu um erro ao gerar o arquivo XML de relatório: {ex}')
-
-def enviar_boletos(mat_prefix, cot_prefix):
-    try:
-        contatos = Database().pega_contatos_db(mat_prefix, cot_prefix)
-        for contato in contatos:
-            if envioPausado:
-                logging.info('Envio de e-mails pausado.')
-                break
-            nome = contato['nome']
-            mat = contato['mat']
-            cot = contato['cot']
-            unidade = pegaUnidade(mat)
-            email = contato['email']
-            emailsUnidade = emailsPorUnidade().get(unidade, [])
-            mensagem_template = lerTemplate('msg.html')
-            mensagem = mensagem_template.substitute(PERSON_NAME=nome.title(), LINK=f"{link}{contato['token']}")
-            assunto = f"Seu BOLETO SMREDE - Unidade {unidade} chegou!!!"
-            enviado = enviarEmail(email, assunto, mensagem, emailsUnidade)
-            if enviado:
-                Database().atualizar_envio(mat)
-            else:
-                logging.error(f"Falha ao enviar e-mail para {nome} ({email})")
-    except Exception as ex:
-        logging.error(f"Ocorreu um erro ao enviar os boletos: {ex}")
-
-if __name__ == '__main__':
-    # Parâmetros para filtrar os contatos
-    mat_prefix = None
-    cot_prefix = None
-
-    # Executando a função para enviar boletos
-    enviar_boletos(mat_prefix, cot_prefix)
+except Exception as ex:
+    logging.error('Ocorreu um erro ao enviar e-mails: %s', ex)
